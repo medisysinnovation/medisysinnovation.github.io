11d761ab18651ef26bd36ace69fec59c9daef6e2
Chris Lu Yi <chris_lu@medinno.onmicrosoft.com>
2021-01-08 15:29:40 +0800

    add test sample case

--- packages/test/__tests__/test.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const test = require('..');
-
-describe('@medisys/test', () => {
-    it('needs tests');
-});
--- packages/utils/__tests__/utils.test.js
+++ packages/utils/__tests__/utils.test.js
@@ -1,7 +1,9 @@
-'use strict';
-
-const utils = require('..');
-
-describe('utils', () => {
-    it('needs tests');
+"use strict";
+const moment = require("moment");
+const utils = require("..");
+const { dateTimeCompare } = utils;
+test("dateTimeCompare compare test case 1", () => {
+  const r = dateTimeCompare(moment(), moment());
+  expect(r).toBeLessThanOrEqual(1);
+  expect(r).toBeGreaterThanOrEqual(-1);
 });
