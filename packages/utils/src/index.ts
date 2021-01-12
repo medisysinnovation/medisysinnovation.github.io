ca2a6383d87acfaa57b2f43fc54275490f71331e
Chris Lu Yi <chris_lu@medinno.onmicrosoft.com>
2021-01-12 11:23:51 +0800

    1

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,8 +1,10 @@
-export * from "./helper";
+// export * from "./helper";
 
 export * from "./calculation";
 export * from "./compare";
 export * from "./component";
 export * from "./network";
+
+// export { history } from "./helper";
 
 export * from "./test";
