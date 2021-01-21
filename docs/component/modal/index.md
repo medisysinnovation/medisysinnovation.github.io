c3d592e440ea59c4ef5b4675fa6385070edd5a98
Chris Lu Yi <chris_lu@medinno.onmicrosoft.com>
2021-01-21 17:12:17 +0800

    1

---
+++ docs/component/modal/index.md
@@ -0,0 +1,14 @@
+---
+nav:
+  title: Components
+  path: /components
+group:
+  title: Modal
+  path: /modal
+---
+
+### API
+  
+| Property | Description | Type | Default | Version |
+| --- | --- | --- | --- | --- |
+| triggerUnsavedChangesWarning | Enable dirty form check | boolean | true |  |
---
+++ packages/component/src/modal/modal.tsx
@@ -0,0 +1,79 @@
+import React, {
+  useEffect,
+  ReactChild,
+  ReactNode,
+  createContext,
+  useState,
+  useRef,
+  MouseEvent,
+} from 'react';
+import { Modal, Select } from 'antd';
+import { useBoolean, useMouse, useEventListener } from 'ahooks';
+
+import { ModalProps } from 'antd/lib/Modal';
+
+export interface MIModalProps extends ModalProps {
+  triggerUnsavedChangesWarning?: boolean;
+}
+const MIModal: React.FC<MIModalProps> = ({
+  triggerUnsavedChangesWarning = true,
+  ...props
+}) => {
+  const ref = useRef() as React.MutableRefObject<HTMLInputElement>;
+
+  const getClosetForm = (el: HTMLElement | null) => {
+    let f = el?.closest('.ant-modal-content')?.querySelector('.medisys-form');
+
+    if (!f) {
+      return el?.querySelector('.medisys-form');
+    }
+    return f;
+  };
+  const { onCancel, visible, children, ...restProps } = props;
+  const [state, { toggle, setTrue, setFalse }] = useBoolean(visible);
+  useEffect(() => {
+    const onDiscardForm = (e: FormEvent) => {
+      if (onCancel)
+        //@ts-ignore
+        onCancel(e);
+    };
+    if (visible === true) {
+      setTrue();
+
+      setTimeout(() => {
+        const form = getClosetForm(ref.current);
+        if (ref.current)
+          ref.current.addEventListener('discardform', onDiscardForm);
+      }, 1);
+    } else {
+      setFalse();
+    }
+
+    return () => {
+      if (ref.current)
+        ref.current.removeEventListener('discardform', onDiscardForm);
+    };
+  }, [visible]);
+
+  return (
+    <div>
+      <Modal
+        {...restProps}
+        visible={state}
+        onCancel={e => {
+          const form = getClosetForm(e.currentTarget);
+          if (form && triggerUnsavedChangesWarning) {
+            form.dispatchEvent(new CustomEvent('aboutdiscardform'));
+            return false;
+          } else {
+            if (onCancel) onCancel(e);
+          }
+        }}
+      >
+        <div ref={ref}>{children}</div>
+      </Modal>
+    </div>
+  );
+};
+
+export default MIModal;
