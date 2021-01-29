3146de2f5f108598a203e7c3f335ad6fe0213c2d
Chris Lu Yi <chris_lu@medinno.onmicrosoft.com>
2021-01-29 15:31:30 +0800

    1

--- config/config.js
+++ config/config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   // hash: true,
   title: 'Medisys Lab',
   mode: 'site',
-  base: '/',
+  base: '/medisys',
   publicPath: '/medisys/',
   // exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
   extraBabelPlugins: [
