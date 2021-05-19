f24a0050413b4f935897b2fd4dc4392d946ed1c4
Chris Lu Yi <chrisluyi@gmail.com>
2021-05-19 18:19:36 +0800

    1

--- packages/notification/src/firebase/index.ts
+++ packages/notification/src/firebase/index.ts
@@ -1,4 +1,4 @@
-import firebase from 'firebase/app';
+import firebase from 'firebase';
 import 'firebase/messaging';
 import { FirebaseOptions } from '@firebase/app-types';
 import request from 'umi-request';
