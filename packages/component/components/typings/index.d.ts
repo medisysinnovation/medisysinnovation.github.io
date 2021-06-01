b1904550e02e8aabb537376e1bef762ddcee9fd4
Chris Lu Yi <chrisluyi@gmail.com>
2021-06-01 17:18:34 +0800

    1

--- packages/component/components/typings/index.d.ts
+++ packages/component/components/typings/index.d.ts
@@ -1,26 +1,2 @@
-
-import type { HTMLAttributes } from 'react';
-
-declare module '*.css';
-declare module '*.less';
-declare module '*.scss';
-declare module '*.svg';
-
-declare namespace jest {
-  interface Matchers<R> {
-    toMatchRenderedSnapshot(): R;
-  }
-}
-
-declare module 'react' {
-  interface AnchorHTMLAttributes<T> extends HTMLAttributes<T> {
-    disabled?: boolean;
-    key?:string;
-    children?:any;
-    onClick?:any;
-    style?:any
-  }
-  interface HTMLAttributes<HTMLDivElement> extends HTMLDivElement<T> {
-    ref?: any;
-  }
-}
+/// <reference path="custom-typings.d.ts" />
+/// <reference path="custom-element-typings.d.ts" />
--- packages/component/typings.d.ts
+++ packages/component/typings.d.ts
@@ -1,23 +1,8 @@
-import type { HTMLAttributes } from 'react';
-
 declare module '*.css';
 declare module '*.less';
 
 declare namespace jest {
   interface Matchers<R> {
     toMatchRenderedSnapshot(): R;
-  }
-}
-
-declare module 'react' {
-  interface AnchorHTMLAttributes<T> extends HTMLAttributes<T> {
-    disabled?: boolean;
-    key?:string;
-    children?:any;
-    onClick?:any;
-    style?:any
-  }
-  interface HTMLAttributes<HTMLDivElement> extends HTMLDivElement<T> {
-    ref?: any;
   }
 }
