a256b9f3402f9a391c3dd049ef79ab6339e25b23
Chris Lu Yi <chrisluyi@gmail.com>
2021-06-01 16:46:39 +0800

    1

--- packages/component/components/typings/index.d.ts
+++ packages/component/components/typings/index.d.ts
@@ -1,4 +1,26 @@
+
+import type { HTMLAttributes } from 'react';
+
 declare module '*.css';
 declare module '*.less';
 declare module '*.scss';
 declare module '*.svg';
+
+declare namespace jest {
+  interface Matchers<R> {
+    toMatchRenderedSnapshot(): R;
+  }
+}
+
+declare module 'react' {
+  interface AnchorHTMLAttributes<T> extends HTMLAttributes<T> {
+    disabled?: boolean;
+    key?:string;
+    children?:any;
+    onClick?:any;
+    style?:any
+  }
+  interface HTMLAttributes<HTMLDivElement> extends HTMLDivElement<T> {
+    ref?: any;
+  }
+}
