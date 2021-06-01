5df488c2314c899a549558f92f9faa06c345aa67
Chris Lu Yi <chrisluyi@gmail.com>
2021-06-01 16:40:48 +0800

    1

--- packages/component/components/typings/index.d.ts
+++ packages/component/components/typings/index.d.ts
@@ -1,20 +1,4 @@
-import type { HTMLAttributes } from 'react';
-
 declare module '*.css';
 declare module '*.less';
 declare module '*.scss';
 declare module '*.svg';
-
-
-declare module 'react' {
-  interface AnchorHTMLAttributes<T> extends HTMLAttributes<T> {
-    disabled?: boolean;
-    key?:string;
-    children?:any;
-    onClick?:any;
-    style?:any
-  }
-  interface HTMLAttributes<HTMLDivElement> extends HTMLDivElement<T> {
-    ref?: any;
-  }
-}
