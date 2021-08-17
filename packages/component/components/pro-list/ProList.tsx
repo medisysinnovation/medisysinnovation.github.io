4d697143d4b49fea2fd6e84d9652d595a3608bc6
Chris Lu Yi <chrisluyi@gmail.com>
2021-08-17 10:53:20 +0800

    fix(pro-list): export pro list props type

--- packages/component/components/pro-list/ProList.tsx
+++ packages/component/components/pro-list/ProList.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ProList from '@ant-design/pro-list';
 import { ProListProps } from '@ant-design/pro-list/es/index';
+export declare type MIProListTypeProps<
+  RecordType extends Record<string, any>,
+  U extends Record<string, any> = Record<string, any>
+> = ProListProps<RecordType, U> & {
+  test: string;
+};
 const MIProList = <
   RecordType extends Record<string, any>,
   U extends Record<string, any> = Record<string, any>
 >(
-  props: ProListProps<RecordType, U>,
+  props: MIProListTypeProps<RecordType, U>,
 ) => {
   return <ProList<RecordType, U> {...props} />;
 };
+
 export default MIProList;
